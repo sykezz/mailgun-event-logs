1aae934614ac245f959790b9a9470002eaf4ce6a
sykez <hello@sykez.com>
2021-10-03 02:36:13 +0800

    Exception handling for requests and KeyError

--- mailgun_events.py
+++ mailgun_events.py
@@ -10,54 +10,62 @@ class MailgunEvents:
     def __init__(self):
         load_dotenv()
         self.log_date = pendulum.yesterday("Asia/Kuala_Lumpur")
+        self.timestamp = pendulum.now().format("Y-MM-DD-HHmmss")
         self.apikey = os.getenv("MAILGUN_APIKEY")
         self.domain = os.getenv("MAILGUN_DOMAIN")
-        self.limit = int(os.getenv("MAILGUN_LIMIT"))
+        self.limit = int(os.getenv("MAILGUN_LIMIT", 300))
         self.log_dir = os.getenv("LOG_DIR")
         self.total_items = 0
         self.page = 0
-        self.timestamp = pendulum.now().format("Y-MM-DD-HHmmss")
+
+    # Mailgun request handling
+    def req(self, url, options={}):
+        try:
+            response = requests.get(url, auth=("api", self.apikey), params=options)
+            response.raise_for_status()  # Raise 4xx errors
+            return response
+        except requests.exceptions.HTTPError as e:
+            raise SystemExit(e)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
 
     # Request events (first page) from Mailgun
-    def request_first_page(self):
-        return requests.get(
+    def req_first_page(self):
+        response = self.req(
             os.getenv("MAILGUN_URL") + "/v3/" + self.domain + "/events",
-            auth=("api", self.apikey),
-            params={
+            {
                 "begin": self.log_date.to_rfc822_string(),
                 "ascending": "yes",
                 "limit": self.limit,
             },
         )
-
-    # Request for the next page of events
-    def request_next_page(self, url):
-        return requests.get(url, auth=("api", self.apikey))
+        return response
 
     # Get and return all event logs from Mailgun
     def get_events(self):
-
         # First page
-        response = self.request_first_page()
-        items = response.json()["items"]
-        self.page += 1
-        self.save_events(items)
-        next_page = response.json()["paging"]["next"]
-        self.total_items = len(items)
-        sys.stdout.write(f"Events fetched ({str(len(items))}). Page: {str(self.page)}" + "\n")
+        processed = self.process_events(self.req_first_page())
 
         # Next pages
-        while len(items) == self.limit:  # Item limit
-            print("next page")
-            response = self.request_next_page(next_page)
-            items = response.json()["items"]
-            self.page += 1
-            self.save_events(items)
-            next_page = response.json()["paging"]["next"]
-            self.total_items += len(items)
-            sys.stdout.write(f"Events fetched ({str(len(items))}). Page: {str(self.page)}" + "\n")
+        while processed["count"] == self.limit:  # Item limit
+            processed = self.process_events(self.req(processed["next_page"]))
 
         sys.stdout.write(f"Done. Total pages: {str(self.page)}. Total items: {str(self.total_items)}")
+
+    def process_events(self, events):
+        # Get items and next page token from response
+        try:
+            items = events.json()["items"]
+            next_page = events.json()["paging"]["next"]
+        except KeyError as kee:
+            sys.stdout.write("No events returned.")
+            sys.exit()
+
+        self.page += 1
+        self.total_items += len(items)
+        self.save_events(items)
+        sys.stdout.write(f"Events fetched ({str(len(items))}). Page: {str(self.page)}" + "\n")
+        return {"count": len(items), "next_page": next_page}
 
     # Save event logs into file
     def save_events(self, events):
